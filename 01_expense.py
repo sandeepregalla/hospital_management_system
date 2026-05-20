4da8952b91bbcdf9c2406f95d44a119be2a4601c
sandeepregalla <sandeepregalla14@gmail.com>
2026-05-20 11:53:20 +0530

    updated expense file

--- 01_expense.py
+++ 01_expense.py
@@ -75,6 +75,7 @@ def menu():
     while True:
         print("\n===== EXPENSE TRACKER =====")
 
+        # Expense Tracker..
         print("1. Add Expense")
         print("2. View Expenses")
         print("3. Total Expense")
